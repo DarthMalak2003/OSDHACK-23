5b728e4d4641201a06c3ba8537e1210984f52b5c
Aviral Jain <aviraljain0204@gmail.com>
2023-04-22 15:05:44 +0530

    Driver Code Updated

--- driver.cpp
+++ driver.cpp
@@ -4,13 +4,13 @@ using namespace std;
 void display()
 {
     cout << ".-------------------------------------." << endl;
+    cout << "|  o    o    o                        |" << endl;
+    cout << "|  Pl   Pa   St                       |" << endl;
     cout << "|                                     |" << endl;
     cout << "|                                     |" << endl;
-    cout << "|                                     |" << endl;
-    cout << "|                                     |" << endl;
-    cout << "|                                     |" << endl;
-    cout << "|                                     |" << endl;
-    cout << "|                                     |" << endl;
+    cout << "|          ---------------            |" << endl;
+    cout << "|         | o  -------  o |           |" << endl;
+    cout << "|          ---------------            |" << endl;
     cout << "|                                     |" << endl;
     cout << "|                                     |" << endl;
     cout << ".-------------------------------------." << endl;

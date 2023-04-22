8570b7be1435baf16cf4408f54f5d096c8d4d944
Apurva3101 <131478582+Apurva3101@users.noreply.github.com>
2023-04-22 14:27:10 +0530

    Committed Credentials File

---
+++ credentials.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    string pw;
+    int login=0;
+    while (login < 3)
+    {
+        cout<< "Enter your user name"<<endl;
+        cin>> name;
+        cout<< "Enter your password"<<endl;
+        cin>> pw;
+       // if (name ==
+
+    }
+
+    if (login==3)
+    {
+         cout<< "Invalid, try again later"<<endl;
+    }
+        return 0;
+}
